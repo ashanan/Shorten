d5b953377a56cd297e5e6288179934f7c03c462e
ashanan <ashanan@gmail.com>
2012-01-08 17:08:01 -0500

    Corrected path for form submission.

--- php/index.php
+++ php/index.php
@@ -3,7 +3,7 @@
         <title></title>
     </head>
     <body>
-        <form id="url-shortener" method="POST" action="shorten/php/shortener.php">
+        <form id="url-shortener" method="POST" action="shortener.php">
             <label name="long-url" for="long-url">URL</label>
             <input type="text" name="long-url" />
             <input type="submit" />
