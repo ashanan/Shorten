e73f81abbff2cb6e230ae0b61615313281677bfc
root <root@gooseisland.ashanan.com>
2012-01-08 16:02:32 -0500

    Added entry page for php version.

---
+++ php/index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <form id="url-shortener" method="POST" action="shorten/php/shortener.php">
+            <label name="long-url" for="long-url">URL</label>
+            <input type="text" name="long-url" />
+            <input type="submit" />
+        </form>
+    </body>
+</html>
