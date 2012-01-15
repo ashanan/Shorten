cbee096d42bc575c099e98d19f37bc70a0c7bb14
ashanan <ashanan@gmail.com>
2012-01-15 17:51:01 -0500

    Url generation works.  Storing in DB works.

--- php/shortener.php
+++ php/shortener.php
@@ -2,12 +2,43 @@
 include('config.php');
 //print_r($_POST['full-url']);
 
-function generateURL(){
-		
+function generateURL(&$pdo){
+	$validURLchars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+
+	try{
+		$stmt = $pdo->query("SELECT count(*) FROM urls;");
+		$row = $stmt->fetch(PDO::FETCH_ASSOC);
+		$count = $row['count(*)'];
+		//echo 'row: ' . print_r($row, TRUE) . '<br>';
+		$url = "";
+		$baseLength = strlen($validURLchars);
+		//echo 'count, base: ' . $count . ', ' . $baseLength . '<br>';
+		do{
+			$divisor = floor($count / $baseLength);
+			//echo 'divisor: ' . $divisor . '<br>';
+			if($divisor > 0){
+				$url .= $validURLchars[$divisor - 1];
+				$count = $count % $baseLength;
+			}
+			else{
+				$url .= $validURLchars[$count % $baseLength];
+				break;
+			}
+			
+		}
+		while($count > 0);
+	}	
+	catch(PDOException $e){
+		print "<br><br>Error: " . print_r($e,TRUE);
+		die();
+	}
+	
+	return $url;
 }
 
 try{
 	$pdo = new PDO('mysql:host=' . $SERVER  . ';dbname='. $DATABASE, $USER, $PASSWORD);
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	//TODO: use prepared statements
 	$statement = $pdo->query("SELECT count(*) FROM urls WHERE full_url = '" . $_POST['full-url'] . "';");
 	$row = $statement->fetch(PDO::FETCH_ASSOC);
@@ -20,8 +51,11 @@ try{
 		echo 'short: ' . $row['short_url'];
 	} 
 	else{
-		$short_url = generateURL();
+		$statement->closeCursor();
+		$short_url = generateURL($pdo);
+		echo 'url: ' .$short_url;
 		$query = "INSERT INTO urls (full_url, short_url) values('" . $_POST['full-url']  . "','" . $short_url  . "');";
+		$pdo->query($query);
 	}
 	$pdo = null;
 }
