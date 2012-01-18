69a678ca9b4b9abfbb110abfcbb2c3c6d94c2d66
ashanan <ashanan@gmail.com>
2012-01-17 23:23:56 -0500

    Replaced queris with prepared statements in redirect.php

--- php/redirect.php
+++ php/redirect.php
@@ -5,12 +5,12 @@ include('config.php');
 try{
 	$pdo = new PDO('mysql:host=' . $SERVER  . ';dbname='. $DATABASE, $USER, $PASSWORD);
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	//TODO: use prepared statements
-	$statement = $pdo->query("SELECT full_url FROM urls WHERE short_url = '" . $_GET['id'] . "';");
 
-	$row = $statement->fetch(PDO::FETCH_ASSOC);
+	$statement = $pdo->prepare("SELECT full_url FROM urls WHERE short_url = ?");
+	$statement->execute(array($_GET['id']));
+
+	$row = $statement->fetch();
 	if($row){
-	//	print_r($row);
 		header('Location: ' .  $row['full_url']);
 	}
 	else{
