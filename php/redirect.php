5538cae2a90533086c273ca6675575ed9b582ba3
Avner Shanan <ashanan@gmail.com>
2016-09-03 12:20:24 -0400

    Make short_url comparison case sensitive

--- php/redirect.php
+++ php/redirect.php
@@ -6,7 +6,7 @@ try{
 	$pdo = new PDO('mysql:host=' . $SERVER  . ';dbname='. $DATABASE, $USER, $PASSWORD);
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-	$statement = $pdo->prepare("SELECT full_url FROM urls WHERE short_url = ?");
+	$statement = $pdo->prepare("SELECT full_url FROM urls WHERE BINARY short_url = ?");
 	$statement->execute(array($_GET['id']));
 
 	$row = $statement->fetch();
